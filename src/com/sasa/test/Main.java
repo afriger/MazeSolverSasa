8a7cdce111c5d1b5d441f9a8c4905a8a3fcbb1e3
afriger <afriger@outlook.com>
2018-08-10 15:46:44 +0300

    first

---
+++ src/com/sasa/test/Main.java
@@ -0,0 +1,27 @@
+package com.sasa.test;
+
+import com.sasa.test.depth_first.Viewer;
+import com.sasa.test.maze_solver.MazeGenerator;
+
+public class Main
+{
+  /*  enum Kaka
+    {
+        Z(5);
+        int  z;
+        Kaka(int z)
+        {
+            this.z=z;
+        }
+
+    }*/
+
+    public static void main(String[] args)
+    {
+        //Viewer
+        MazeGenerator
+                .main(args);
+
+
+    }
+}
